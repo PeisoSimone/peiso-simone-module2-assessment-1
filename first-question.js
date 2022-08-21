9573d7fe1ce37f8ad159a8b6d64b7c0c1e1b9b31
Peiso Simone <simonepeiso@gmail.com>
2022-08-21 18:07:06 +0200

    First Solution

---
+++ first-question.js
@@ -0,0 +1,7 @@
+/*Log your information (names, institution, and Github username) into the console log with a statement that introduces you, e.g. “My name is Tsakane, a student from Xawe University of Technology and my Github username is ”*/
+
+Details("Peiso Simone", "IT Varsity/MTN Academy", "PeisoSimone")
+
+function Details(names, instituition, Github_username){
+    return console.log("My name is "+names+", a student from "+instituition+" and my Github username is "+Github_username)
+}
